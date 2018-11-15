6d4c7e4702f06c3c75fa0eb105f27066b797643a
SVasiliy <vasiliy.sharapov@gmail.com>
2018-11-15 10:53:00 -0600

    stopwatch v1 complete

--- src/stopwatch/Stopwatch.js
+++ src/stopwatch/Stopwatch.js
@@ -3,17 +3,33 @@ import React, { Component } from 'react';
 
 class Stopwatch extends Component {
     state = { lapse: 0, running: false }
-
+    handleRunClick = () => {
+        this.setState((state) => {
+            if (state.running) {
+                clearInterval(this.timer);
+            } else {
+                const startTime = Date.now() - this.state.lapse;
+                this.timer = setInterval(() => {
+                    this.setState({ lapse: Date.now() - startTime });
+                }, 0);
+            }
+            return { running: !state.running };
+        });
+    }
+    handleClearClick = () => {
+        clearInterval(this.timer);
+        this.setState({ lapse: 0, running: false });
+    }
     render() {
         const { lapse, running } = this.state;
         return (
             <div>
                 <p>{lapse} ms</p>
                 <button
-                    onClick={null}
+                    onClick={this.handleRunClick}
                     className="stopwatchbutton">{running ? 'stop' : 'start'}</button>
                 <button
-                    onClick={null}
+                    onClick={this.handleClearClick}
                     className="stopwatchbutton">clear</button>
             </div>
         );
