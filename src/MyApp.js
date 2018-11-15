1c02bf0e31708bcd53f5ebb3ab57a435760045f6
SVasiliy <vasiliy.sharapov@gmail.com>
2018-11-14 21:10:00 -0600

    Button with event handling

---
+++ src/MyApp.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react';
+import './ui.css';
+
+class MyApp extends Component {
+    state = { eventCount: 0 }
+
+    handleButtonClick = () => {
+        this.state.eventCount = this.state.eventCount + 1;
+        this.forceUpdate();
+    }
+
+    render() {
+        const state = this.state;
+
+        return (
+            <div>
+                <p>button clicked {state.eventCount} times</p>
+                <p>
+                    <button
+                        onClick={this.handleButtonClick}
+                        className="button">click</button>
+                </p>
+            </div>
+        );
+    }
+}
+
+export default MyApp;
