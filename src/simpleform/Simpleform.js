e53dddc5dfcb7dc1b3c28eec5badc027f64ac73b
SVasiliy <vasiliy.sharapov@gmail.com>
2018-11-16 21:53:32 -0600

    simpleform step 3 disable submit button dynamically

--- src/simpleform/Simpleform.js
+++ src/simpleform/Simpleform.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 
 class Simpleform extends Component {
-    state = { lapse: 0 }
+    state = { error: 'null' }
     handleSubmit = (event) => {
         event.preventDefault();
        // console.log(event.target.elements.MyName.value);
     }
 
+    getErrorMessage = (value) => {
+        if (value.length < 5) {
+            return 'Value is less than 5 characters';
+        }
+        return null;
+    }
+
+    handleChange = (event) => {
+        const { value } = event.target;
+        this.setState({ error: this.getErrorMessage(value) });
+    }
+
     render() {
+        const { error } = this.state;
         return (
             <form onSubmit={this.handleSubmit}>
-                <p> Name {this.lapse}
-                    <input type="text" name="MyName" />
-                    <button type="submit">submit</button>
+                <p> Name
+                    <input
+                        type="text"
+                        name="MyName"
+                        onChange={this.handleChange} />
+                    <button disabled={Boolean(error)} type="submit">submit</button>
                 </p>
             </form>
         );
