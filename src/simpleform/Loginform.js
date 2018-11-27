060e2eef369ad817e970988fedcd2e48840c582c
SVasiliy <vasiliy.sharapov@gmail.com>
2018-11-26 22:02:22 -0600

    Link Loginform with /login route

--- src/simpleform/Loginform.js
+++ src/simpleform/Loginform.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import './Login.css';
 
 class Loginform extends Component {
 
