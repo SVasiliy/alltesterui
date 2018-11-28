c07189298a2f69a679ddc0dab4c2adf6d76aeb68
SVasiliy <vasiliy.sharapov@gmail.com>
2018-11-28 12:01:51 -0600

    add app session state - step 1

--- src/App.js
+++ src/App.js
@@ -6,13 +6,24 @@ import './App.css';
 import Routes from './Routes';
 
 class App extends Component {
-    state = { loading: false, compex: null };
 
-    handleSelect = (eventKey) => {
-        this.setState({ compex: eventKey });
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAuthenticated: false,
+        };
+    }
+
+    userHasAuthenticated = (authenticated) => {
+        this.setState({ isAuthenticated: authenticated });
     }
 
     render() {
+        const childProps = {
+            isAuthenticated: this.state.isAuthenticated,
+            userHasAuthenticated: this.userHasAuthenticated,
+        };
+
         return (
             <div className="App container">
                 <Navbar fluid collapseOnSelect>
@@ -30,7 +41,7 @@ class App extends Component {
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
-                <Routes />
+                <Routes childProps={childProps} />
             </div>
         );
     }
