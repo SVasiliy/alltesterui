31a4ff06de9b442b10eb0fa6ac21157c9ac643c2
SVasiliy <vasiliy.sharapov@gmail.com>
2018-12-03 15:56:19 -0600

    redux step 1

---
+++ src/redux/reducers.js
@@ -0,0 +1,13 @@
+import {ADD_TOKEN} from './actions'
+
+const initialState = {
+    token: null
+}
+
+function alltesterApp(state, action) {
+    if (typeof state === 'undefined') {
+      return initialState
+    }
+  ​
+    return state
+}
