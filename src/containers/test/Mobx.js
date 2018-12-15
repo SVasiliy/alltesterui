b9e458e5ac07e7c33f8c036653e5dc13b1604aa8
SVasiliy <vasiliy.sharapov@gmail.com>
2018-12-15 08:39:30 -0600

    mobx - add devtools

--- src/containers/test/Mobx.js
+++ src/containers/test/Mobx.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import Devtools from 'mobx-react-devtools';
 import { observer } from 'mobx-react';
 import { mobxState } from '../../App';
 
@@ -11,6 +12,7 @@ const Mobx = observer(class Mobx extends Component {
     render() {
         return (
             <div>
+                <Devtools />
                 Counter: {mobxState.counter} <br />
                 <button onClick={this.handleInc}> + </button>
                 <button onClick={this.handleDec}> - </button>
