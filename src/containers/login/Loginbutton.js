220efd1dc059badb8feb95f85906ed413c74eaa7
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-14 20:43:57 -0600

    bug fix for prev commit

--- src/containers/login/Loginbutton.js
+++ src/containers/login/Loginbutton.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { NavItem } from 'react-bootstrap';
-import { Redirect } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { observer, inject } from 'mobx-react';
 
