de9881f9ce3be6649d887b73dad4b61275ea6ce3
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-19 17:22:01 -0600

    DummyKeycloaker step 1

--- src/containers/keycloak/DummyKeycloaker.js
+++ src/containers/keycloak/DummyKeycloaker.js
@@ -6,11 +6,18 @@ const DummyKeycloaker = inject('mobxstate')(observer(class DummyKeycloaker exten
 
     handleInc = () => { this.props.mobxstate.increment(); }
     handleDec = () => { this.props.mobxstate.decrement(); }
+    doAlert = () => { alert(111); }
 
     render() {
         return (
             <div>
                 dummy keycloak tester
+                <br />
+                <hr />
+                <br />
+                /api/premium &nbsp; <button onClick={this.doAlert}>GET call</button>
+                <br />
+                <hr />
             </div>
         );
     }
