1ed7d70956a8f0f4a25b3b0b0757a21dd8ad4906
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-23 08:39:23 -0600

    DummyKeycloaker step 8 code refactor

--- src/containers/keycloak/DummyKeycloaker.js
+++ src/containers/keycloak/DummyKeycloaker.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 import { observer, inject } from 'mobx-react';
 import PremiumViewer from './PremiumViewer';
 import JwtViewer from './JwtViewer';
-import { decodeJWT } from '../../components/utils';
 
 const DummyKeycloaker = inject('mobxstate')(observer(class DummyKeycloaker extends Component {
 
     saveApiPremium = (val) => { this.props.mobxstate.saveApiPremium(val); }
-    decodeJWTs = () => { decodeJWT(this.props.mobxstate.token.access_token); }
 
     callPremium = () => {
         if (this.props.mobxstate.token === null) {
@@ -17,8 +15,6 @@ const DummyKeycloaker = inject('mobxstate')(observer(class DummyKeycloaker exten
             const AuthStr = 'Bearer '.concat(this.props.mobxstate.token.access_token);
             axios.get(`${process.env.REACT_APP_APIURL}/premium`, { headers: { Authorization: AuthStr } })
                 .then((response) => {
-            // If request is good...
-                    console.log(response.data);
                     this.saveApiPremium(response.data);
                 })
                 .catch((error) => {
@@ -33,11 +29,11 @@ const DummyKeycloaker = inject('mobxstate')(observer(class DummyKeycloaker exten
                 <b>dummy keycloak tester</b>
                 <br />
                 <hr />
+                access_token: <JwtViewer />
+                <hr />
                 /api/premium &nbsp; <button onClick={this.callPremium}>GET call</button>
                 <PremiumViewer />
                 <hr />
-                output test &nbsp; <button onClick={this.decodeJWTs}>click me</button>
-                <JwtViewer />
                 <br />
                 <hr />
             </div>
