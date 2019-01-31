de295e85dea7f32bedcd562bd09095ab7532f1a0
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-31 08:47:20 -0600

    css testing (float:both)

--- src/containers/test/cssplay/CssPlay.js
+++ src/containers/test/cssplay/CssPlay.js
@@ -22,6 +22,9 @@ class CssPlay extends Component {
                         <div className="div1">box1</div>
                         <div className="div1">box2</div>
                     </div>
+                    <div className="column4">
+                      column 4 (with float:both)
+                    </div>
                 </div>
             </div>
         );
