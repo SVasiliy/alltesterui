c0b17e631178ec9488b2e82bfdeab6004e506ce6
SVasiliy <vasiliy.sharapov@gmail.com>
2019-02-22 09:39:39 -0600

    expose props version

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Home.css';
 
 export default class Home extends Component {
-    state = { temp: false }
+    state = { version: null }
+
+    componentDidMount() {
+        axios.get(`${process.env.REACT_APP_APIURL}/version`)
+            .then((response) => {
+                this.setState({ version: response.data });
+            });
+    }
+
     render() {
         return (
             <div className="Home">
                 <div className="lander">
                     <h1>Alltester</h1>
                     <p>A simple app to test everything</p>
+                    <p>props v. {this.state.version}</p>
                 </div>
             </div>
         );
