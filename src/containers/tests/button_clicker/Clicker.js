47c81cfc0ee1ce42a8ef92dd1f8f4174e77eb928
SVasiliy <vasiliy.sharapov@gmail.com>
2019-03-13 10:57:23 -0500

    conponent layout step 1

--- src/containers/tests/button_clicker/Clicker.js
+++ src/containers/tests/button_clicker/Clicker.js
@@ -13,6 +13,20 @@ class Clicker extends Component {
     render() {
         return (
             <div>
+
+                <div className="clearfix">
+                    <div className="box" style={{ backgroundColor: '#bbb' }}>
+                        <p>Some text inside the box.</p>
+                    </div>
+                    <div className="box" style={{ backgroundColor: '#ccc' }}>
+                        <p>Some text inside the box.</p>
+                    </div>
+                    <div className="box" style={{ backgroundColor: '#ddd' }}>
+                        <p>Some text inside the box.</p>
+                    </div>
+                </div>
+
+
                 <ButtonClicker />
 
                 <br />
--- src/containers/tests/button_clicker/FunctionCaller.js
+++ src/containers/tests/button_clicker/FunctionCaller.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './ClickerCSS.css';
 import { printSomething } from './utils';
 
-const FunctionCaller = () => (<button
-    onClick={printSomething}
-    className="button">print log message</button>);
+const FunctionCaller = () => (<div className="clickDiv">
+    <button
+        onClick={printSomething}
+        className="button">print log message
+    </button>
+</div>);
 
 export default FunctionCaller;
