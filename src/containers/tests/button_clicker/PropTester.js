6005f259934d1be5782cd382dec2067c8ab9120d
SVasiliy <vasiliy.sharapov@gmail.com>
2019-03-14 10:00:48 -0500

    bug fix for previous commit

--- src/containers/tests/button_clicker/PropTester.js
+++ src/containers/tests/button_clicker/PropTester.js
@@ -4,6 +4,9 @@ import PropTesterChild from './PropTesterChild';
 /*
 not only change of states triggers re-render
 but also if a property passed to the child is changed - re-render is triggered
+
+note: nope, in the case described above "this.forceUpdate()" is needed and it is a discourage practice.
+Better use state for such needs.
 */
 class PropTester extends Component {
 
@@ -12,7 +15,7 @@ class PropTester extends Component {
     render() {
         return (<div>
             <button
-                onClick={() => { this.propObj = { value: 'test value 2' }; }}
+                onClick={() => { this.propObj = { value: 'test value 2' }; this.forceUpdate(); }}
                 className="button">change ChildProp</button>
             <PropTesterChild testProp={this.propObj} />
         </div>);
